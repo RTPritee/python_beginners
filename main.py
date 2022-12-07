493336c9adfbbfbf855409045ef3b277eb0c6e0a
RTPritee <pritee593@gmail.com>
2022-12-07 12:36:24 +0600

    start python

---
+++ main.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    return x+y
+
+print(add(1,2))
+
+first_name = "Rehnuma" #variable name can not have space
+print(first_name)
+
+is_online = False # boolean , case sensetive , can not use small f
+print(is_online)
